afba87c021a8c7c8c3be80f90f4216e39eae4c2e
capstxr <114504520+capstxr@users.noreply.github.com>
2024-01-31 11:58:39 +0200

    Add files via upload

--- src/ts/App.tsx
+++ src/ts/App.tsx
@@ -1,30 +1,11 @@
-import React from 'react';
-
 import '../css/App.scss';
 import '../css/colors.css';
-
-import {
-    BrowserRouter as Router,
-    Routes,
-    Route
-} from 'react-router-dom';
 
 import Home from './pages/home/home';
 
 function App() {
     return (
-
-    <Router>
-        <Routes>
-            <Route
-                path='/'
-                element={
-                    <Home name='Helsinki'/>
-                }
-            />
-        </Routes>
-    </Router>
-
+        <Home name='Helsinki'/>
     );
 }
 
